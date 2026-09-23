1b7aeac6d3a0d0d64a0d21cc18fd8d392d20b4a4
divya2006-cse <busanaboyina.lakshmidivya130079@marwadiuniversity.ac.in>
2026-09-23 12:09:46 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,5 @@
+PS C:\Users\divya\AppData\Local\Programs\Microsoft VS Code> & C:\Users\divya\AppData\Local\Programs\Python\Python313\python.exe "c:/Users/divya/OneDrive/Desktop/Practical 7/program.py"
+Enter N: 4
+Enter coins: 2 4 6 7
+Number of ways: 2
+PS C:\Users\divya\AppData\Local\Programs\Microsoft VS Code> 
